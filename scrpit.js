21424c38c679deae7febe6ef324dffed313085c5
Saitilink <menu.thomas1@gmail.com>
2024-10-16 15:33:57 +0200

    Bon on va checker ça nous même faut pas le dire

--- scrpit.js
+++ scrpit.js
@@ -4,7 +4,7 @@ let liste = JSON.parse(localStorage.getItem('cliqueListe')) || [];
 
 document.querySelectorAll('.cliquable').forEach(div => {
     div.addEventListener('click',(e) => {
-        const clique = e.currentTarget.id
+        const clique = e.currentTarget.id;
         if (clique) {
             liste.push(clique);  // Ajoute l'ID à la liste
             localStorage.setItem('cliqueListe', JSON.stringify(liste));  // Met à jour le LocalStorage
@@ -15,26 +15,27 @@ document.querySelectorAll('.cliquable').forEach(div => {
     })
 })
 
-emailjs.init("xk4qPaDVln9ySWPAM");
-
-const serviceID = 'service_1z3jtct'
-const templateID = 'template_mq5fx6a'
-
-
-
-
-function afficher() {
-    console.log('idhuefo')   
+function afficher() {   
     console.log(JSON.parse(localStorage.getItem('cliqueListe')));
-    emailjs.send(serviceID, templateID, {
-        user_name:"Thomas",
-        message: liste
-})
-.then(function(response) {
-    console.log('SUCCESS!', response.status, response.text);
-}, function(error) {
-    console.log('FAILED...', error);
-});
-    liste = []
+    
 }
     
+function reset(){
+    liste =[];
+    localStorage.setItem('cliqueListe', JSON.stringify(liste))
+
+}
+
+function ajoutHeure(){
+    const valDeb = document.getElementById("begining").value;
+    const valFin = document.getElementById("end").value;
+    liste.push(valDeb);
+    liste.push(valFin);  // Ajoute l'ID à la liste
+    localStorage.setItem('cliqueListe', JSON.stringify(liste));
+}
+
+function pushVal(){
+    const val = document.getElementById("rangeInput").value;
+    liste.push(val)
+    localStorage.setItem('cliqueListe', JSON.stringify(liste));
+}
